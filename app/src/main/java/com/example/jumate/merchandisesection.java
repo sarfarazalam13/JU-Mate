de16dfba29b7a45fac9d5d353f98a69ad3e5664a
Anushka <anushkaagrawal2010@gmail.com>
2023-04-24 02:35:30 +0530

    updated Features

---
+++ app/src/main/java/com/example/jumate/merchandisesection.java
@@ -0,0 +1,14 @@
+package com.example.jumate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class merchandisesection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchandisesection);
+    }
+}
